1fed2fd6c030bd5842bcaffef6e07213b4642629
Vinothkumar R <vinothkumar@gmail.com>
2026-08-19 19:34:58 +0530

    update

---
+++ JavaScript/Tasks/Day03/assets/calculation.js
@@ -0,0 +1,96 @@
+let a = 20;
+let b = 5;
+
+console.log("Add", a + b);
+
+console.log("Sub", a - b);
+
+console.log("Multiple", a * b);
+
+console.log("Divide", a / b);
+
+console.log("Remainder", a % b);
+
+
+
+let score = 100;
+
+
+score += 20;
+console.log("Score = ", score);
+
+score -= 20;
+console.log("Score = ", score);
+
+score *= 3;
+console.log("Score = ", score);
+
+score /= 10;
+console.log("Score = ", score);
+
+
+
+
+let age = 22;
+
+
+console.log(age == 22);
+
+console.log(age === 22);
+
+console.log(age > 18);
+
+console.log(age < 18);
+
+console.log(age >= 18);
+
+console.log(age <= 18);
+
+
+
+
+let age1 = 22;
+
+let hasId = true;
+
+
+console.log(age1 >= 18 && hasId);
+
+console.log(age1 < 18 || hasId);
+
+console.log(!hasId);
+
+
+
+
+
+
+let count = 10;
+
+
+
+count++;
+console.log("first count+", count);
+
+count++;
+console.log("second count+", count);
+
+count--;
+console.log("first count-", count);
+
+count--;
+console.log("second count-", count);
+
+
+
+let number = 10;
+
+a = number % 2;
+
+if (a===0) {
+    console.log("Even")
+}
+
+else {
+    console.log("Odd")
+}
